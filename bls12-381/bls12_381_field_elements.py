b2fa47b9c35d726b3b9753a21558a7a631247db4
Onur İnanç <e191322@metu.edu.tr>
2023-04-25 12:03:41 +0300

    implement FQ6, FQ12

--- bls12-381/bls12_381_field_elements.py
+++ bls12-381/bls12_381_field_elements.py
@@ -130,8 +130,121 @@ class FQ6():
         self.c1 = u1
         self.c2 = u2
 
-    def __add__(self):
-        print("not impl yet!")
+    def __add__(self, other):
+        assert(isinstance(other, FQ6))
+        return FQ6(self.c0 + other.c0, self.c1 + other.c1, self.c2 + other.c2)
+
+    def __radd__(self, other):
+        assert(isinstance(other, FQ6))
+        return self + other
+
+    def __sub__(self, other):
+        assert(isinstance(other, FQ6))
+        return FQ6(self.c0 - other.c0, self.c1 - other.c1, self.c2 - other.c2)
+    
+    def __rsub__(self, other):
+        assert(isinstance(other, FQ6))
+        return FQ6(other.c0 - self.c0, other.c1 - self.c1, other.c2 - self.c2)
+
+    def __mul__(self, other):
+        assert(isinstance(other, FQ6))
+        t0 = self.c0 * other.c0
+        t1 = self.c0 * other.c1 + self.c1 * other.c0
+        t2 = self.c0 * other.c2 + self.c1 * other.c1 + self.c2 * other.c0
+        t3 = self.c1 * other.c2 + self.c2 * other.c1
+        t3 = t3.mul_nonres()
+        t4 = self.c2 * other.c2
+        t4 = t4.mul_nonres()
+        return FQ6(t0 + t3, t1 + t4, t2)
+
+    def __rmul__(self, other):
+        assert(isinstance(other, FQ6))
+        return self * other
+
+    def mul_nonres(self):
+        return FQ6(self.c2.mul_nonres(), self.c0, self.c1)
+
+    def invFQ6(self):
+        mul_nonres1 = self.c1 * self.c2
+        mul_nonres2 = self.c2 * self.c2
+        t0 = self.c0 * self.c1 - mul_nonres1.mul_nonres()
+        t1 = mul_nonres2.mul_nonres() - self.c0 * self.c1
+        t2 = self.c1 * self.c1 - self.c0 * self.c2
+        mul_nonres3 = self.c2 * t1
+        mul_nonres4 = self.c1 * t2
+        inverse_temp = self.c0 * t0 + mul_nonres3 + mul_nonres4
+        factor = inverse_temp.invFQ2()
+        return FQ6(t0 * factor, t1 * factor, t2 * factor)
+
+    def __eq__(self, other):
+            assert(isinstance(other, FQ6))
+            return self.c0 == other.c0 and self.c1 == other.c1 and self.c2 == self.c2
+        
+    def __neq__(self, other):
+        assert(isinstance(other, FQ6))
+        return not self == other
+
+    def __neg__(self):
+        return FQ6(-self.c0, -self.c1, -self.c2)
+
+    def __repr__(self):
+        return "(" + self.c0.__str__() + "," + self.c1.__str__() + "," + self.c2.__str__() + ")"
+
+class FQ12():
+    def __init__(self, u0, u1):
+        assert(isinstance(u0, FQ6))
+        assert(isinstance(u1, FQ6))
+        self.c0 = u0
+        self.c1 = u1
+    
+    def __add__(self, other):
+        assert(isinstance(other, FQ12))
+        return FQ12(self.c0 + other.c0, self.c1 + other.c1)
+
+    def __radd__(self, other):
+        assert(isinstance(other, FQ12))
+        return self + other
+
+    def __sub__(self, other):
+        assert(isinstance(other, FQ12))
+        return FQ12(self.c0 - other.c0, self.c1 - other.c1)
+
+    def __rsub__(self, other):
+        assert(isinstance(other, FQ12))
+        return FQ12(other.c0 - self.c0, other.c1 - self.c1)
+
+    def __mul__(self, other):
+        assert(isinstance(other, FQ12))
+        mul_nonres1 = self.c1 * other.c1
+        return FQ12(self.c0 * other.c0 + mul_nonres1.mul_nonres(), self.c1 * other.c0 + self.c0 * other.c1)
+
+    def __rmul__(self, other):
+        assert(isinstance(other, FQ12))
+        return self * other
+
+
+    def mul_nonres(self):
+        print("No need for FQ12!")
+
+    def invFQ12(self):
+        mul_nonres1 = self.c1 * self.c1
+        inverse_temp = self.c0 * self.c0 - mul_nonres1.mul_nonres()
+        factor = inverse_temp.invFQ6()
+        return FQ12(self.c0 * factor, -self.c1 * factor)
+
+    def __eq__(self, other):
+            assert(isinstance(other, FQ12))
+            return self.c0 == other.c0 and self.c1 == other.c1
+        
+    def __neq__(self, other):
+        assert(isinstance(other, FQ12))
+        return not self == other
+
+    def __neg__(self):
+        return FQ12(-self.c0, -self.c1)
+
+    def __repr__(self):
+        return "(" + self.c0.__str__() + "," + self.c1.__str__() + ")"
 
 
 # Extended Euclidean Algorithm to find an inverse of a field element
@@ -148,18 +261,53 @@ def inv(a, n):
 
 a = FQ(1)
 b = FQ(1)
+c = FQ(3)
+d = FQ(4)
+e = FQ(2)
+f = FQ(8)
 
-c = FQ2(a, b)
-d = FQ2(a, b)
+u = FQ2(a, b)
+v = FQ2(c, d)
+t = FQ2(e, f)
 
-res = c.invFQ2()
-print(c == d)
-print(c != d)
-print(c == -d)
+x = FQ6(u, v, t)
+y = FQ6(t, v, u)
+
+res = x * y
+res1 = y * x
 print(res)
 print(res.c0)
 print(res.c1)
+print(res.c2)
+print(res1)
+print(res1.c0)
+print(res1.c1)
+print(res1.c2)
 
-total = res.c0 + res.c1
-print(total)
+res2 = x + y
+print(res2)
 
+res3 = x - y
+print(res3)
+
+res4 = x.invFQ6()
+print(res4)
+
+res5 = y.mul_nonres()
+print(res5)
+print("#################")
+
+zz = FQ12(x, y)
+zy = FQ12(x, -y)
+
+result = zz * zy
+print(result)
+
+result1 = zz + zy
+print(result1)
+
+result2 = zz - zy
+print(result2)
+
+result3 = zz.invFQ12()
+print(result3)
